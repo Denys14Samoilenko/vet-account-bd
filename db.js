84cef99524788da28a4c3e7a6e5f281d2637a512
Denys <d.samoilenko92@gmail.com>
2024-11-15 13:39:19 +0200

    fix: change urls to db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(`${process.env.MONGO_URI}/test`, { 
+mongoose.connect(`${process.env.MONGO_URI}`, { 
 	useNewUrlParser: true, 
 	useUnifiedTopology: true,
 	ssl: true 
